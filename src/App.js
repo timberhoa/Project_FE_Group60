4922ba58bfe650332562c1a57fb48ade6d83f57b
Truong2k9 <lnhuttruong89@gmail.com>
2024-06-18 16:46:25 +0700

    cập nhật code navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import TodoList from "./components/Todolist";
+import React, {useEffect, useState} from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import loadingImg from "./assets/loader.gif";
+import { CssBaseline } from "@material-ui/core";
+import Navbar from "./components/Navbar/Navbar";
+import {commerce} from "./lib/commerce";
 
-function App() {
-  return <TodoList/>;
-}
+
+
+const App = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
+
+  useEffect(() => {
+    fetchProducts().then(r => fetchProducts());
+  }, []);
+  return (
+      <div>
+        {products.length > 0 ? (
+            <>
+              <Router>
+                <div style={{ display: "flex" }}>
+                  <CssBaseline />
+                  <Navbar
+                      totalItems={cart.total_items}
+                      handleDrawerToggle={handleDrawerToggle}
+                  />
+                </div>
+              </Router>
+            </>
+        ) : (
+            <div className="loader">
+              <img src={loadingImg} alt="Loading" />
+            </div>
+        )}
+      </div>
+  );
+};
 
 export default App;
